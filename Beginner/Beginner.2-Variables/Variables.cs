ba98f1d0e6d1d5b62342d4fc70af9a100ba008cc
AKonicki26 <anne.konicki@mymail.champlain.edu>
2025-06-30 17:09:04 -0400

    Create project for Variables

---
+++ Beginner/Beginner.2-Variables/Variables.cs
@@ -0,0 +1,12 @@
+﻿namespace Beginner._2_Variables;
+
+public class Variables
+{
+    public static int Main()
+    {
+        // create a variable of the integer type called number, and set it equal to 7
+        int number = 7;
+        
+        return number;
+    }
+}
---
+++ Tests/Beginner.2-Variables.Tests/Beginner.2-Variables.Tests.cs
@@ -0,0 +1,10 @@
+namespace Beginner.Variables.Tests;
+
+public class Tests
+{
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
